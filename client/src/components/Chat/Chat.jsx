29c5e5b9f3d063f724c734776f81f2c981229f66
jrucl2d <ysk789465@gmail.com>
2020-10-07 22:04:28 +0900

    ClientSide : socket.io emmiting event added

--- client/src/components/Chat/Chat.jsx
+++ client/src/components/Chat/Chat.jsx
@@ -1,7 +1,25 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import queryString from "query-string";
+import io from "socket.io-client";
 import "./Chat.css";
 
-function Chat() {
+let socket;
+
+function Chat({ location }) {
+  const [name, setName] = useState("");
+  const [room, setRoom] = useState("");
+  const SERVERLOC = "localhost:8000";
+
+  useEffect(() => {
+    const { room, name } = queryString.parse(location.search);
+
+    socket = io(SERVERLOC);
+
+    setName(name);
+    setRoom(room);
+
+    socket.emit("join", { name, room });
+  }, [SERVERLOC, location.search]);
   return (
     <div>
       <h1>chat</h1>
