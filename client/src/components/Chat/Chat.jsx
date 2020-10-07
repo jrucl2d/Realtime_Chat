eb69608bd4e19c8ece1980583ade8c9bf3fc514e
jrucl2d <ysk789465@gmail.com>
2020-10-07 23:19:30 +0900

    Basic message event done

--- client/src/components/Chat/Chat.jsx
+++ client/src/components/Chat/Chat.jsx
@@ -8,6 +8,9 @@ let socket;
 function Chat({ location }) {
   const [name, setName] = useState("");
   const [room, setRoom] = useState("");
+  const [message, setMessage] = useState("");
+  const [messages, setMessages] = useState([]);
+
   const SERVERLOC = "localhost:8000";
 
   useEffect(() => {
@@ -25,9 +28,33 @@ function Chat({ location }) {
       socket.off(); // socket 자체를 닫아버림
     };
   }, [SERVERLOC, location.search]);
+
+  useEffect(() => {
+    socket.on("message", (message) => {
+      setMessages([...messages, message]);
+    });
+  }, [messages]);
+
+  const sendMessage = (e) => {
+    e.preventDefault();
+    if (message) {
+      socket.emit("sendMessage", message, () => setMessage("")); // callback으로 메시지 전송 후 메시지 input 비우기
+    }
+  };
+
+  console.log(message, messages);
+
   return (
     <div>
-      <h1>chat</h1>
+      <div className="outerContainer">
+        <div className="container">
+          <input
+            value={message}
+            onChange={(e) => setMessage(e.target.value)}
+            onKeyPress={(e) => (e.key === "Enter" ? sendMessage(e) : null)}
+          />
+        </div>
+      </div>
     </div>
   );
 }
--- server/app.js
+++ server/app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const socketIO = require("socket.io");
 const app = express();
 const PORT = process.env.PORT || 8000;
+const { addUser, removeUser, getUser, getUsersInRoom } = require("./users");
 
 app.get("/", require("./routes")); // router
 
@@ -13,11 +14,31 @@ const io = socketIO(server);
 
 // io를 통해서 새 유저(socket)이 연결됨을 파악
 io.on("connection", (socket) => {
-  console.log("new user connected!!");
+  socket.on("join", ({ name, room }, callback) => {
+    const { error, user } = addUser({ id: socket.id, name, room }); // addUser에서 error을 반환하거나 user을 반환함
 
-  // 파라미터 뒤에 , callback을 넣어서 error handling도 가능
-  socket.on("join", ({ name, room }) => {
-    console.log(name, room);
+    if (error) return callback(error); // error handling
+
+    // 현재 들어간 사람에게 보여지는 admin 메시지
+    socket.emit("message", {
+      user: "admin",
+      text: `${user.name}, welcome to the room ${user.room}`,
+    });
+    // 현재 들어간 사람을 제외한 방 사람에게 보내지는 admin 메시지
+    socket.broadcast.to(user.room).emit("message", {
+      user: "admin",
+      text: `${user.name} has joined the room!`,
+    });
+
+    socket.join(user.room); // socket을 socket.io의 'room'에 join 시킴
+    callback(); // error가 없음
+  });
+
+  // 프론트에서 유저가 적은 메시지가 서버로 오면 처리 후 프론트로 이벤트 emit(전체 전송)
+  socket.on("sendMessage", (message, callback) => {
+    const user = getUser(socket.id);
+    io.to(user.room).emit("message", { user: user.name, text: message });
+    callback();
   });
 
   // 해당 유저(socket)이 연결을 해제함
